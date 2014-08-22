5be95fd1b0db63dd3a4c15aa77acc7105af9dc5d
Gabor Csardi <csardi.gabor@gmail.com>
2014-08-22 11:08:29 -0400

    Add manual

--- R/dotenv-package.r
+++ R/dotenv-package.r
@@ -2,6 +2,28 @@
 ## --------------------------------------------------------------------
 #' Load configuration parameters from .env into environment variables
 #'
+#' It has become a practice to store configuration parameters related
+#' to a project, in a hidden file called \code{.env}, in the working
+#' directory of a project, and then set them as environment variables.
+#'
+#' This package loads the variables defined in the \code{.env} file
+#' in the current working directory (as reported by \code{getwd}),
+#' and sets them as environment variables.
+#'
+#' This happens automatically when the \code{dotenv} package is loaded,
+#' so the typical use-case is to just put a `library(dotenv)` code at the
+#' beginning of your R script.
+#'
+#' Alternatively a \code{dotenv::load_dot_env()} call can be used
+#' to load variables from arbitrary files.
+#'
+#' The format of the \code{.env} file is also a valid unix shell
+#' file format, so e.g. in \code{bash} the environment variables
+#' can also be set by running \code{source .env}.
+#'
+#' See \code{\link{load_dot_env}} for the exact file format.
+#'
+#' @seealso load_dot_env
 #' @docType package
 #' @name dotenv-package
 #' @import magrittr
@@ -17,9 +39,50 @@ NULL
 
 #' Load environment variables from the specified file
 #'
-#' @param file The name of the file to load from.
+#' Load variables defined in the given file, as environment
+#' variables.
+#'
+#' @details
+#' The file is parsed line by line, and line is expected
+#' to have one of the following formats:
+#' \preformatted{VARIABLE=value
+#' VARIABLE2="quoted value"
+#' VARIABLE3='another quoted variable'
+#' export EXPORTED="exported variable"
+#' export EXPORTED2=another}
+#'
+#' In more details:
+#' \itemize{
+#'   \item A leading \code{export} is ignored, to keep the file
+#'      compatible with Unix shells.
+#'   \item No whitespace is allowed right before or after the
+#'      equal sign, again, to promote compatilibity with Unix shells.
+#'   \item No multi-line variables are supported currently. The
+#'      file is strictly parsed line by line.
+#'   \item Unlike for Unix shells, unquoted values are \emph{not}
+#'      terminated by whitespace.
+#' }
+#'
+#' It is suggested to keep the file in a form that is parsed the
+#' same way with \code{dotenv} and \code{bash} (or other shells).
+#'
+#' @param file The name of the file to use.
 #' @export
 #' @importFrom falsy %||%
+#'
+#' @examples
+#' # Remove, if it exists
+#' Sys.unsetenv("dotenvexamplefoo")
+#' Sys.getenv("dotenvexamplefoo")
+#'
+#' # Load from a file
+#' tmp <- tempfile()
+#' cat("dotenvexamplefoo=bar\n", file = tmp)
+#' load_dot_env(tmp)
+#' Sys.getenv("dotenvexamplefoo")
+#'
+#' # Clean up
+#' unlink(tmp)
 
 load_dot_env <- function(file = ".env") {
 
